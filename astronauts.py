19bae4a7af25eee91c5464418bf582fba426c5d1
vulpum <ak0s.szab0621@gmail.com>
2024-02-03 14:32:31 +0100

    second commit

--- astronauts.py
+++ astronauts.py
@@ -1,3 +1,8 @@
+"""
+This program tells the user the first 3 months astronauts has born in from the 'astronauts.csv' file.
+"""
+
+
 def get_birth_months():
     months = []
     with open('astronauts.csv', 'r', encoding='utf-8') as file:
@@ -8,13 +13,13 @@ def get_birth_months():
 
 def main():
     birth_months = get_birth_months()
-    print(birth_months)
     months = []
     for month in range(1, 13):
         months.append(birth_months.count(str(month)))
-    print(months)
     months_sorted = sorted(months)
-    print()
+    print('The 3 months most astronauts has born in:')
+    for counter in range(1, 4):
+        print(f'{months.index(months_sorted[-counter]) + 1}. month: {round(months_sorted[-counter] / len(birth_months) * 100, 1)}%')
 
 
 main()
